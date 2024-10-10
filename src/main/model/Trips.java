483b5e5b8495c883bcc2050e4d55b3f1bf2d6a4d
cthakral <cthakral@students.cs.ubc.ca>
2024-10-09 19:48:05 -0700

    Tested the Trips.java

--- src/main/model/Trips.java
+++ src/main/model/Trips.java
@@ -11,47 +11,53 @@ public class Trips {
 
 // EFFECTS: contructs a trip with city, country, type of the trip and list of itinerary.
     public Trips(String city, String country, String tripType){
-        
+        this.city=city;
+        this.country=country;
+        this.tripType=tripType;
+        itinerary=new ArrayList<DestinationItinerary>();
     }
+
     //MODIFIES: this
     //EFFECTS:sets the City to the new provided city.
     public void setCity(String city) {
-    
+        this.city=city;
     }
     //MODIFIES: this
     //EFFECTS:sets the country to the new provided country.
     public void setCountry(String country) {
-        
+       this.country=country;
     }
 
     //MODIFIES: this
     //EFFECTS: sets the trip type to the new provided trip type.
     public void setTripType(String tripType){
-    
+        this.tripType=tripType;
     }
 
     //MODIFIES: this
     //EFFECTS: add the given activity to our list of destination itinerary.
     public void addDestinationItineraries(DestinationItinerary activity){
-    //stub
+        itinerary.add(activity);
     }
     
     public String getCity() {
-    return null;
+    return this.city;
 
     }
 
     public String getCountry() {
-    return null;
+    return this.country;
 
     }
 
     public List<DestinationItinerary> getDestinationItinerary() {
-    return null;
+    return this.itinerary;
     }
 
     public String getTripType() {
-    return null;
+    return this.tripType;
     }
 
 }
+
+
