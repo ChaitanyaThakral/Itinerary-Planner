890cc83f628d03b5890f6d88488e0a8bb1fc6a5e
cthakral <cthakral@students.cs.ubc.ca>
2024-10-10 14:01:36 -0700

    Added tests for Budget.java

---
+++ src/test/model/BudgetTest.java
@@ -0,0 +1,31 @@
+package model;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BudgetTest {
+    private Budget budget;
+
+    @BeforeEach
+    void runBefore(){
+        budget=new Budget (1000.0,0.0);
+    }
+
+    @Test
+    public void testIncreaseBudgetLimit(){
+        budget.increaseBudgetLimit(100);
+        assertEquals(1100,budget.getBudgetLimit());
+    }
+
+    @Test 
+    public void testIncreaseCurrentExpenditure(){
+        budget.increaseCurrentExpenditure(100);
+        assertEquals(100,budget.getCurrentExpenditure());
+    }
+
+    @Test
+    public void testCheckBudget(){
+        assertFalse(budget.checkBudget());
+    }
+    
+}
