c905a3c8ef99bc27fd6ef235477bff9d933b61c0
cthakral <cthakral@students.cs.ubc.ca>
2024-10-10 14:31:00 -0700

    Test added after adding budget field in Activity.java

--- src/test/model/ActivityTest.java
+++ src/test/model/ActivityTest.java
@@ -1,18 +1,21 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.List;
-import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ActivityTest {
     private Activity a1;
+    private Activity a2;
+    private Budget b1;
 
     @BeforeEach
     void runBefore(){
+        b1=new Budget(1000, 0);
         a1= new Activity("Surfing", "Vancouver", "2024-09-10",
-        60,"10:00 AM", "Did surfing with family", 100.0, true);
+        60,"10:00 AM", "Did surfing with family", 100.0, true,b1);
+        a2= new Activity("Eating", "Vancouver", "2024-09-10",
+        20,"11:00 AM", "Ate Noodles with family", 30.0, true,new Budget(10, 0));
     }
 
     @Test
@@ -30,37 +33,37 @@ public class ActivityTest {
     }
 
     @Test
-    public void setDate(){
+    public void testSetDate(){
        a1.setDate("2024-09-11");
        assertEquals("2024-09-11", a1.getDate());
     }
 
     @Test
-    public void setDuration(){
+    public void testSetDuration(){
         a1.setDuration(100);
         assertEquals(100,a1.getDuration());
     }
     
     @Test
-    public void setTime(){
+    public void testSetTime(){
         a1.setTime("11:00 AM");
         assertEquals("11:00 AM",a1.getTime());
     }
 
     @Test
-    public void setDescription(){
+    public void testSetDescription(){
         a1.setDescription("Went to Surf");
         assertEquals("Went to Surf",a1.getDescription());
     }
     
     @Test
-    public void setCost(){
+    public void testSetCost(){
         a1.setCost(90.00);
         assertEquals(90.00,a1.getCost());
     }
 
     @Test
-    public void setStatus(){
+    public void testSetStatus(){
         a1.setStatus(false);
         assertFalse(a1.getStatus());
 
@@ -68,5 +71,15 @@ public class ActivityTest {
         assertTrue(a1.getStatus());
     }
 
+    @Test
+    public void testBudgetCheck(){
+        assertTrue(a1.budgetCheck());
+        assertFalse(a2.budgetCheck());
+    }
+
+    @Test
+    public void testGetBudget(){
+        assertEquals(b1,a1.getBudget());
+    }
     
 }
