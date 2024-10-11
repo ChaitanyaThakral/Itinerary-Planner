2a3aaa48cb53ad9d5ae23351072437bff5fcf1f2
cthakral <cthakral@students.cs.ubc.ca>
2024-10-11 16:29:44 -0700

    Checkstyle Improved for BudgetTest.java

--- src/test/model/BudgetTest.java
+++ src/test/model/BudgetTest.java
@@ -1,5 +1,7 @@
 package model;
+
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,27 +9,27 @@ public class BudgetTest {
     private Budget budget;
 
     @BeforeEach
-    void runBefore(){
-        budget=new Budget (1000.0,0.0);
+    void runBefore() {
+        budget = new Budget(1000.0, 0.0);
     }
 
     @Test
-    public void testIncreaseBudgetLimit(){
+    public void testIncreaseBudgetLimit() {
         budget.increaseBudgetLimit(100);
-        assertEquals(1100,budget.getBudgetLimit());
-    }
-
-    @Test 
-    public void testIncreaseCurrentExpenditure(){
-        budget.increaseCurrentExpenditure(100);
-        assertEquals(100,budget.getCurrentExpenditure());
+        assertEquals(1100, budget.getBudgetLimit());
     }
 
     @Test
-    public void testbudgetExceed(){
+    public void testIncreaseCurrentExpenditure() {
+        budget.increaseCurrentExpenditure(100);
+        assertEquals(100, budget.getCurrentExpenditure());
+    }
+
+    @Test
+    public void testbudgetExceed() {
         assertFalse(budget.budgetExceed());
         budget.increaseCurrentExpenditure(2000);
         assertTrue(budget.budgetExceed());
     }
-    
+
 }
