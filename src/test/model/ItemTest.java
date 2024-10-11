45707c6be0f277279774439f5ee10b3cefc169b4
cthakral <cthakral@students.cs.ubc.ca>
2024-10-11 16:34:43 -0700

    Improved CheckStyle for ItemTest.java

--- src/test/model/ItemTest.java
+++ src/test/model/ItemTest.java
@@ -1,30 +1,32 @@
 package model;
+
 import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemTest {
     private Item i1;
-    
+
     @BeforeEach
-    public void runBefore(){
-        i1= new Item("Passport", false);
+    public void runBefore() {
+        i1 = new Item("Passport", false);
     }
 
     @Test
-    public void testItem(){
-        assertEquals("Passport",i1.getName());
+    public void testItem() {
+        assertEquals("Passport", i1.getName());
         assertFalse(i1.getStatus());
     }
 
     @Test
-    public void testSetName(){
+    public void testSetName() {
         i1.setName("Sunglasses");
-        assertEquals("Sunglasses",i1.getName());
+        assertEquals("Sunglasses", i1.getName());
     }
 
     @Test
-    public void testSetStatus(){
+    public void testSetStatus() {
         i1.setStatus(true);
         assertTrue(i1.getStatus());
     }
