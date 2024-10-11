6b4eea999450f4faef9abff2e9d46518c9aa2c05
cthakral <cthakral@students.cs.ubc.ca>
2024-10-11 09:44:34 -0700

    Checkstyle improved

--- src/main/model/Trips.java
+++ src/main/model/Trips.java
@@ -10,7 +10,8 @@ public class Trips {
     private String tripType;
     private List<DestinationItinerary> itinerary; 
 
-// EFFECTS: contructs a trip with city, country, type of the trip and list of itinerary.On creation the itinerary list is empty.
+// EFFECTS: contructs a trip with city, country, type of the trip and list of itinerary
+//.On creation the itinerary list is empty.
     public Trips(String city, String country, String tripType) {
         this.city = city;
         this.country = country;
