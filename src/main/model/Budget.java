169f1aaedb73d8e134b853986245c4612ed2c212
cthakral <cthakral@students.cs.ubc.ca>
2024-10-22 20:45:04 -0700

    Added Specifications for toJson in Budget class and Budget now implements the Writeable Interface.

--- src/main/model/Budget.java
+++ src/main/model/Budget.java
@@ -1,49 +1,63 @@
 package model;
 
+import org.json.JSONObject;
+
+import persistence.Writeable;
+
 /* Represents a budget with a budget limit the amount the person has decided to
 spend on a particualr activity and current Expenditure the amount the user has already spent on the activity.
 Budget has currentExpenditure: the total amount of money user has spent on the activity so far,
  *  budgetLimit: the maximum amount of money limit for a activity. */
-public class Budget {
+
+public class Budget implements Writeable {
     private double currentExpenditure;
     private double budgetLimit;
 
-    //EFFECTS: constructs a budget with its limit
+    // EFFECTS: constructs a budget with its limit
     // and expenditure of what one has spent till now(intial value at 0.0).
-    public Budget(double budgetLimit,double currentExpenditure) {
-        this.budgetLimit = budgetLimit;          
-        this.currentExpenditure = currentExpenditure;  
+    public Budget(double budgetLimit, double currentExpenditure) {
+        this.budgetLimit = budgetLimit;
+        this.currentExpenditure = currentExpenditure;
     }
 
-    //EFFETCS: return the amount of money spent till now.
+    // EFFETCS: return the amount of money spent till now.
     public double getCurrentExpenditure() {
         return this.currentExpenditure;
     }
 
-    //EFFETCS: retun the budget limit set for the activity.
+    // EFFETCS: retun the budget limit set for the activity.
     public double getBudgetLimit() {
         return this.budgetLimit;
     }
 
-    //MODIFIES:this
-    //EFFECTS: increase the budget limit by the given amount.
+    // MODIFIES:this
+    // EFFECTS: increase the budget limit by the given amount.
     public void increaseBudgetLimit(double amount) {
         this.budgetLimit = this.budgetLimit + amount;
     }
 
-    //MODIFIES:this
-    //EFFECTS: increase the current expenditure by the given amount.
+    // MODIFIES:this
+    // EFFECTS: increase the current expenditure by the given amount.
     public void increaseCurrentExpenditure(double amount) {
         this.currentExpenditure = this.currentExpenditure + amount;
     }
 
-    //EFFECTS: check if the user has exceeded the limit set for the activity.
-    //If budget is exceeded it returns true, otherwise returns false. 
+    // EFFECTS: check if the user has exceeded the limit set for the activity.
+    // If budget is exceeded it returns true, otherwise returns false.
     public Boolean budgetExceed() {
         if (this.currentExpenditure > this.budgetLimit) {
-            return true; 
+            return true;
         } else {
             return false;
         }
     }
+
+    // REQUIRES: the Budget instance should not be null.
+    // EFFECTS: returns a JSONObject representation of this Budget instance
+    // with budgetLimit and CurrentExpenditure.
+    @Override
+    public JSONObject toJson() {
+        return null;
+    }
+
 }
