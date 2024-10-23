e732fb9bc11194cfeed59137fda76230353b29ba
cthakral <cthakral@students.cs.ubc.ca>
2024-10-22 19:43:06 -0700

    Added Specifications for toJson in Trip class and Trip now implements the Writeable Interface.

--- src/main/model/Trips.java
+++ src/main/model/Trips.java
@@ -3,11 +3,15 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONObject;
+
+import persistence.Writeable;
+
 /*Represents a trip with city, country, type of the trip and list of itinerary associated with that trip 
  * Trip specifies a city where the user is going to , country where the user is going to, type of the trip 
  * solo, buisness or family trip, itinerary of the trip of what all the activities user may have for his trip
 */
-public class Trips {
+public class Trips implements Writeable{
     private String city;
     private String country;
     private String tripType;
@@ -62,6 +66,14 @@ public class Trips {
 
     public String getTripType() {
         return this.tripType;
+    }
+
+    //REQUIRES: the trip instance should not be null.
+    //EFFECTS: returns a JSONObject representation of this Trips instance, including all the details and fields like 
+    //city, country, type of the trip and list of itinerary.
+     @Override
+    public JSONObject toJson() {
+        return null;
     }
 
 }
--- src/main/persistence/Writeable.java
+++ src/main/persistence/Writeable.java
@@ -4,9 +4,9 @@ import org.json.JSONObject;
 
 public interface Writeable {
     // Referenced from the JsonSerialization Demo
-    //https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
-    
-     // EFFECTS: returns this as JSON object
+    // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+    // EFFECTS: returns this as JSON object
     JSONObject toJson();
-    
+
 }
